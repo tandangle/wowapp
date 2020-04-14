eae949e71debd8c603e3ec427e8d211ced391ad1
unknown <tandangle76@gmail.com>
2020-04-14 01:37:35 -0400

    able to pull price for individual items from AH and sort auctions by descending unit price

--- index.js
+++ index.js
@@ -1,52 +1,21 @@
-// const axios = require('axios');
-
-// axios.post(`https://us.battle.net/oauth/token`, {
-//     auth: {
-//         username: "8170587803004052b43eb6583a7a82ca",
-//         password: "7sFw5yUDviIfc77w1lHTIcb1Wyvu1IdQ"
-//     },
-//     data: "grant_type=client_credentials"
-// })
-// .then(function(response){
-//     console.log(response);
-// })
-// .catch(function(error) {
-//     console.log("I've run into an error");
-//     console.log(error);
-// });
-
-// const BlizzAPI = require('blizzapi');
-
-// const BnetApi = new BlizzAPI({
-//   region: 'us',
-//   clientId: '8170587803004052b43eb6583a7a82ca',
-//   clientSecret: '7sFw5yUDviIfc77w1lHTIcb1Wyvu1IdQ',
-// });
-
-// BnetApi.query("/wow/connected-realm/index?namespace=dynamic-us&:region=us")
-//   .then(function (response) {
-//       console.log(response)
-//   })
-//   .catch(function (error) {
-//       console.log(error)
-//   });
-
 const axios = require("axios");
 
-axios.request({
-    url: "/oauth/token",
-    method: "post",
-    baseURL: "https://us.battle.net",
-    auth: {
-      username: "8170587803004052b43eb6583a7a82ca",
-      password: "7sFw5yUDviIfc77w1lHTIcb1Wyvu1IdQ"
-    },
-    data: {
-      "grant_type": "client_credentials"
-    }
-  }).then(respose => {
-    console.log(respose);  
+axios.post("https://us.battle.net/oauth/token?grant_type=client_credentials&client_id=8170587803004052b43eb6583a7a82ca&client_secret=7sFw5yUDviIfc77w1lHTIcb1Wyvu1IdQ")
+  .then(function(response){
+    axios.get(`https://us.api.blizzard.com/data/wow/connected-realm/73/auctions?namespace=dynamic-us&:region=us`, {headers:{Authorization: "Bearer " + response.data.access_token}})
+        .then(function(response){
+            var tidespray = response.data.auctions.filter(function(auction){
+              return auction.item.id === 152576
+            });
+              tidespray.sort(function(a,b){
+                return a.unit_price - b.unit_price
+              })
+            console.log(tidespray)
+        })
+        .catch(function(error){
+          console.log(error)
+        })
   })
   .catch(function(error){
-      console.log(error)
-  }); 
+    console.log(error)
+  })
